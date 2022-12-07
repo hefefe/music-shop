69db25c37045a3dde9c9f2845c2b7091ee4a284a
Michal Witczak <michal.witczak666@gmail.com>
2022-12-07 11:56:29 +0100

    initial commit

---
+++ src/main/java/pl/pab/music4u/Music4uApplication.java
@@ -0,0 +1,13 @@
+package pl.pab.music4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Music4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Music4uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pab/music4u/Music4uApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pab.music4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Music4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
